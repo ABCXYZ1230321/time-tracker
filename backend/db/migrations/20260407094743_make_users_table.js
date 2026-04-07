704248070af902f992a5684f6eb99a1a7d41dbbc
Rudra Ponkshe <rudra.ponkshe@protonmail.com>
2026-04-07 16:47:46 +0530

    feat: add db scaffolding
    - rearrange the repository for a better separation of
      concerns among frontend and backend
    - add `docker-compose.yml` file with a postgres service
    - wrote initial knex.js migration to add the `users` table to the
      database

---
+++ backend/db/migrations/20260407094743_make_users_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    // Primary key 'id' with UUID generation
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.string('username').notNullable().unique();
+    table.string('email').notNullable().unique();
+    table.string('password_hash').notNullable();
+    table.timestamps(true, true); // Adds created_at and updated_at with defaults
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+  .dropTable('users');
+};
